95e8da3686087f0bb722333c4f295d0d115e0861
m-bashari-m <m-bashari@outlook.com>
2022-05-27 19:20:22 +0430

    update: completing

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,15 @@
+from abc import abstractmethod
+from pydoc import classname
 import tensorflow as tf
 from tensorflow import keras
 import os
 import tensorflow_hub as hub
 import numpy as np
-from tqdm.notebook import tqdm_notebook as tqn
+from tqdm.notebook import tqdm_notebook as tqdn
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
-import sklearn.metrics as sk_metr
+import keras.backend as K
 
 class ModelCreator():
     def __init__(self, hub_module_url, model_name):
@@ -61,7 +63,6 @@ class ModelCreator():
         check_point = keras.callbacks.ModelCheckpoint(
                                                     filepath=check_point_path,
                                                     monitor='auc',
-                                                    save_best_only=True,
                                                     mode='max')
         
         callbacks = [early_stopping, check_point]
@@ -76,18 +77,16 @@ class ErrorAnalyzer():
         self.classes = classes
         self.model_name = model_name
         self.log_file = os.path.join('./logs/statistic')
-        self.conf_mat = self.calc_confusion_mat()
+        self.conf_mat = self._calc_confusion_mat()
 
-    def calc_confusion_mat(self):
+    def _calc_confusion_mat(self):
         self.lbls = np.array([])
         self.preds = np.array([])
 
-        # tqn may not work
-        for img_batch, lbl_batch in tqn(self.ds):
+        print("Making confusion matrix:")
+        for img_batch, lbl_batch in tqdn(self.ds):
             pred = tf.argmax(self.model(img_batch), axis=-1)
             self.preds = np.concatenate([self.preds, pred.numpy()])
-            # There is some problem
-            # becuase lbl_batch is not one-hot
             self.lbls = np.concatenate([self.lbls, tf.argmax(lbl_batch, axis=-1).numpy()]) 
         
         conf_mat = tf.math.confusion_matrix(self.lbls, self.preds).numpy()
@@ -103,43 +102,32 @@ class ErrorAnalyzer():
         plt.ylabel('Actual')
         plt.xlabel('Predicted')
 
-        dest = os.path.join(self.log_file, 'images', self.model_name+'.jpg')
+        images_dir = os.path.join(self.log_file, 'images')
+        os.makedirs(images_dir, exist_ok=True)
+        dest = os.path.join(images_dir, self.model_name+'.jpg')
         plt.savefig(dest, dpi=150)
         plt.show(block=False)
 
 
     def evaluate_model(self):
-        tp_metric = keras.metrics.TruePositives()
-        tn_metric = keras.metrics.TrueNegatives()
-        fp_metric = keras.metrics.FalseNegatives()
-        fn_metric = keras.metrics.FalsePositives()
-
-        tp_metric.update_state(self.lbls, self.preds)
-        tp = tp_metric.result().numpy()
-
-        tn_metric.update_state(self.lbls, self.preds)
-        tn = tn_metric.result().numpy()
-
-        fp_metric.update_state(self.lbls, self.preds)
-        fp = fp_metric.result().numpy()
-
-        fn_metric.update_state(self.lbls, self.preds)
-        fn = fn_metric.result().numpy()
-
-        # there is some problem with tp and others 
-        # add inde needed
+        print("Calculating error type...")
+        conf_stat = ConfusionStatistic(self.conf_mat, self.classes)
+        
         csv_output = os.path.join(self.log_file, self.model_name+'.csv')
+        print("Writing in log file...")
         with open(csv_output, 'a') as f:
             f.write('color,precision,recall,TP,TN,FP,FN\n')
-            for i in range(len(self.classes)):
+            for i, class_ in enumerate(self.classes):
                 precision, recall = self.get_precision_recall(i)
-                f.write("{},{},{},{},{},{},{}\n".format(self.classes[i],
+                f.write("{},{},{},{},{},{},{}\n".format(class_,
                                                         precision,
                                                         recall,
-                                                        tp,
-                                                        tn,
-                                                        fp,
-                                                        fn))
+                                                        conf_stat.tp[class_],
+                                                        conf_stat.tn[class_],
+                                                        conf_stat.fp[class_],
+                                                        conf_stat.fn[class_]))
+                                                        
+        print("All done. Check log file {}.".format(self.model_name+'.csv'))
 
             
     def get_precision_recall(self, class_num):
@@ -148,19 +136,74 @@ class ErrorAnalyzer():
         return precision, recall
 
 
-def get_train_val_ds(train_dir, val_dir, batch_size=64, img_size=(512,512), seed=42, shuffle=True):
+class ConfusionStatistic():
+    def __init__(self, confusion_mat, classes):
+        self.confusion_mat = confusion_mat
+        self.classes = classes
+        self._tp = self.__true_positive()
+        self._fp = self.__false_positive()
+        self._fn = self.__false_negative()
+        self._tn = self.__true_negative()
+
+    @property
+    def tp(self):
+        return self._tp
+
+    @property
+    def fp(self):
+        return self._fp
+
+    @property
+    def fn(self):
+        return self._fn
+
+    @property
+    def tn(self):
+        return self._tn
+
+    def __true_positive(self):
+        result = dict().fromkeys(self.classes)
+        for i, class_ in enumerate(self.classes):
+            result[class_] = self.confusion_mat[i, i]
+
+        return result
+
+    def __false_positive(self):
+        result = dict().fromkeys(self.classes)
+        for i, class_ in enumerate(self.classes):
+            result[class_] = np.sum(self.confusion_mat[:, i]) - self.tp
+
+        return result
+
+    def __false_negative(self):
+        result = dict().fromkeys(self.classes)
+        for i, class_ in enumerate(self.classes):
+            result[class_] = np.sum(self.confusion_mat[i, :]) - self.tp
+
+        return result
+
+    def __true_negative(self):
+        result = dict().fromkeys(self.classes)
+        for i, class_ in enumerate(self.classes):
+            result[class_] = np.sum(self.confusion_mat) - self.fp - self.fn + self.tp
+
+        return result
+
+
+    
+def get_train_val_ds(train_dir, val_dir,batch_size=32, img_size=(512,512), seed=42, shuffle=True):
     train_ds = keras.utils.image_dataset_from_directory(train_dir,
-                                                    batch_size=batch_size,
                                                     image_size=img_size,
                                                     shuffle=shuffle,
                                                     label_mode='categorical',
+                                                    batch_size=32,
                                                     seed=seed)
 
     val_ds = keras.utils.image_dataset_from_directory(val_dir,
-                                                  batch_size=batch_size,
                                                   image_size=img_size,
                                                   shuffle=shuffle,
                                                   label_mode='categorical',
+                                                  batch_size=32,
                                                   seed=seed)
 
     return train_ds, val_ds
